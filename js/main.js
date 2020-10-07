0fa48bd07feeb07639a5dbdf6f12897f0ad73709
Yougn <shestakovevgeniy11@gmail.com>
2020-10-07 09:50:35 +0300

    Добавил отрисовку главной карточки

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ var MESSAGES = ['Всё отлично!', 'В целом всё неплохо. 
 var NAMES = ['Артем', 'Егор', 'Леночка', 'Максим', 'Захар'];
 var NUMBER = 25;
 var COMMENTS_NUMBER = 10;
+var CARD_IMG_WIDTH = 35;
+var CARD_IMG_HEIGHT = 35;
 
 var getRandomInteger = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
@@ -34,12 +36,12 @@ var createComment = function () {
 };
 
 var getRandomCommentsList = function () {
-  var commentsLists = [];
-  var randomNumber = getRandomInteger(1,NUMBER)
+  var comments = [];
+  var randomNumber = getRandomInteger(1, NUMBER)
   for (var i = 0; i < randomNumber; i++) {
-    commentsLists.push(createComment());
+    comments.push(createComment());
   }
-  return commentsLists;
+  return comments;
 };
 
 var getObjectPicture = function (number) {
@@ -78,9 +80,62 @@ var renderPicture = function (photos) {
 var createFragment = function (photos) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < photos.length; i++) {
-    fragment.appendChild(renderPicture (photos[i]));
+    fragment.appendChild(renderPicture(photos[i]));
   }
   return fragment;
 };
 
 pictureList.appendChild(createFragment(photos));
+
+var bigPicture = document.querySelector('.big-picture');
+bigPicture.classList.remove('hidden');
+
+var renderBigPicture = function (photos) {
+  bigPicture.querySelector('.big-picture__img img').src = photos.url;
+  bigPicture.querySelector('.likes-count').textContent = photos.likes;
+  bigPicture.querySelector('.comments-count').textContent = photos.comments.length;
+  bigPicture.querySelector('.social__caption').textContent = photos.description;
+
+  var renderComments = function () {
+    var pictureComments = bigPicture.querySelector('.social__comments');
+
+    for (var i = 0; i < photos.comments.length; i++) {
+      var newElement = document.createElement('li');
+      newElement.classList.add('social__comment');
+      pictureComments.appendChild(newElement);
+    }
+
+    var renderCommentContents = function () {
+      var pictureComment = pictureComments.querySelectorAll('.social__comment');
+
+      for (var j = 2; j < photos.comments.length; j++) {
+        var newImg = document.createElement('img');
+        newImg.classList.add('social__picture');
+        newImg.src = photos.comments[j].avatar;
+        newImg.alt = photos.comments[j].name;
+        newImg.style.width = CARD_IMG_WIDTH;
+        newImg.style.height = CARD_IMG_HEIGHT;
+        pictureComment[j].appendChild(newImg);
+
+        var newText = document.createElement('p');
+        newText.classList.add('social__text');
+        newText.textContent = photos.comments[j].message;
+        pictureComment[j].appendChild(newText);
+      }
+    }
+    renderCommentContents();
+  }
+  renderComments();
+}
+
+renderBigPicture(photos[0]);
+
+var commentCount = document.querySelector('.social__comment-count');
+commentCount.classList.remove('comments-loader');
+commentCount.classList.add('hidden');
+
+var commentLoader = document.querySelector('.comments-loader');
+commentLoader.classList.add('hidden');
+
+var body = document.querySelector('body');
+body.classList.add('modal-open')
