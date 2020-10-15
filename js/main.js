71be3c63373ae3d9173251e79b51a2a75df4f6e9
Yougn <shestakovevgeniy11@gmail.com>
2020-10-15 21:40:11 +0300

    добавил форму

--- js/main.js
+++ js/main.js
@@ -15,6 +15,12 @@ var NUMBER = 25;
 var COMMENTS_NUMBER = 10;
 var CARD_IMG_WIDTH = 35;
 var CARD_IMG_HEIGHT = 35;
+var BLOCK__WIDTH = 453;
+var STEP = 25;
+var MIN_SIZE = 25;
+var MAX_SIZE = 100;
+var MIN_LENGTH = 1;
+var MAX_LENGTH = 19;
 
 var getRandomInteger = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
@@ -61,8 +67,7 @@ var getObjectsList = function (number) {
   return objectLists;
 };
 
-var photo = getObjectsList(NUMBER);
-console.log(photo);
+var photos = getObjectsList(NUMBER);
 
 var pictureList = document.querySelector('.pictures')
 var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
@@ -77,18 +82,18 @@ var renderPicture = function (photo) {
   return picture;
 };
 
-var createFragment = function (photo) {
+var createFragment = function (photos) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < photo.length; i++) {
-    fragment.appendChild(renderPicture(photo[i]));
+  for (var i = 0; i < photos.length; i++) {
+    fragment.appendChild(renderPicture(photos[i]));
   }
   return fragment;
 };
 
-pictureList.appendChild(createFragment(photo));
+pictureList.appendChild(createFragment(photos));
 
 var bigPicture = document.querySelector('.big-picture');
-bigPicture.classList.remove('hidden');
+// bigPicture.classList.remove('hidden');
 
 var renderBigPicture = function (photo) {
   bigPicture.querySelector('.big-picture__img img').src = photo.url;
@@ -133,7 +138,162 @@ var renderBigPicture = function (photo) {
   commentLoader.classList.add('hidden');
 
   var body = document.querySelector('body');
-  body.classList.add('modal-open')
+  body.classList.add('modal-open');
 }
 
-renderBigPicture(photo[0]);
+renderBigPicture(photos[0]);
+
+
+var uploadFile = document.querySelector('#upload-file');
+
+var showFormImage = function () {
+  var formImage = document.querySelector('.img-upload__overlay');
+  formImage.classList.remove('hidden');
+}
+
+uploadFile.addEventListener('change', function () {
+  showFormImage();
+});
+
+var closeFormImage = function () {
+  var formImage = document.querySelector('.img-upload__overlay');
+  formImage.classList.add('hidden');
+
+  uploadFile.value = '';
+
+  var body = document.querySelector('body');
+  body.classList.remove('modal-open');
+}
+
+var buttonCloseImage = document.querySelector('.img-upload__cancel');
+
+buttonCloseImage.addEventListener('click', function () {
+  closeFormImage();
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Escape') {
+    closeFormImage();
+  }
+});
+
+var sliderPin = document.querySelector('.effect-level__pin');
+var pictureFilter = document.querySelector('.img-upload__preview');
+
+var getSliderPinPosition = function () {
+  var x = sliderPin.offsetLeft;
+  return x;
+};
+
+var getLevelEffect = function () {
+  var level = Math.floor((getSliderPinPosition() * 100) / BLOCK__WIDTH);
+  return level;
+};
+
+var changeEffectInput = function () {
+  var levelValue = document.querySelector('.effect-level__value').value;
+  levelValue = getLevelEffect() + '%';
+  return levelValue;
+}
+
+var regulateLevelEffect = function () {
+
+  if (pictureFilter.classList.contains('effects__preview--chrome')) {
+    var filterChrome = document.querySelector('.effects__preview--chrome')
+    filterChrome.style.filter = 'grayscale(' + getLevelEffect() / 100 + ')';
+  } else if (pictureFilter.classList.contains('effects__preview--sepia')) {
+    var filterSepia = document.querySelector('.effects__preview--sepia')
+    filterSepia.style.filter = 'sepia(' + getLevelEffect() / 100 + ')';
+  } else if (pictureFilter.classList.contains('effects__preview--marvin')) {
+    var filterMarvin = document.querySelector('.effects__preview--marvin')
+    filterMarvin.style.filter = 'invert(' + getLevelEffect() + '%)';
+  } else if (pictureFilter.classList.contains('effects__preview--phobos')) {
+    var filterPhobos = document.querySelector('.effects__preview--phobos')
+    filterPhobos.style.filter = 'blur(' + getLevelEffect() / 100 * 3 + 'px)';
+  } else if (pictureFilter.classList.contains('effects__preview--heat')) {
+    var filterHeat = document.querySelector('.effects__preview--heat')
+    filterHeat.style.filter = 'brightness(' + getLevelEffect() / 100 * 3 + ')';
+  } else if (pictureFilter.classList.contains('effects__preview--none')) {
+    var slider = document.querySelector('.img-upload__effects')
+    slider.classList.add('hidden');
+  }
+
+  changeEffectInput();
+};
+
+sliderPin.addEventListener('mouseup', function () {
+  regulateLevelEffect();
+});
+
+var changeFilterPicture = function () {
+  var radioFilter = document.querySelectorAll('.effects__radio');
+
+  for (var i = 0; i < radioFilter.length; i++) {
+    radioFilter[i].addEventListener('change', function (evt) {
+      var choise = evt.target.value;
+      pictureFilter.classList.add('effects__preview--' + choise);
+      if (pictureFilter.classList.length > 2) {
+        pictureFilter.classList.remove('effects__preview--none');
+        pictureFilter.classList.remove('effects__preview--chrome');
+        pictureFilter.classList.remove('effects__preview--sepia');
+        pictureFilter.classList.remove('effects__preview--marvin');
+        pictureFilter.classList.remove('effects__preview--phobos');
+        pictureFilter.classList.remove('effects__preview--heat');
+        pictureFilter.classList.add('effects__preview--' + choise);
+      }
+    })
+  };
+};
+
+changeFilterPicture();
+
+var changePictureSize = function () {
+  var buttonPlus = document.querySelector('.scale__control--bigger');
+  var buttonMinus = document.querySelector('.scale__control--smaller');
+  var fieldControl = document.querySelector('.scale__control--value');
+  var fieldControlValue = parseInt(fieldControl.value, 10);
+
+  var changeSize = function () {
+    var number = parseInt(fieldControl.value, 10);
+    pictureFilter.style.transform = 'scale(' + number / 100 + ')';
+  };
+
+  buttonPlus.addEventListener('click', function () {
+    if (fieldControlValue < MAX_SIZE) {
+      fieldControlValue += STEP;
+      fieldControl.value = fieldControlValue + "%";
+    } else { return MAX_SIZE + "%" }
+    changeSize();
+  });
+
+  buttonMinus.addEventListener('click', function () {
+    if (fieldControlValue > MIN_SIZE) {
+      fieldControlValue -= STEP;
+      fieldControl.value = fieldControlValue + "%";
+    } else { return MIN_SIZE + "%" }
+    changeSize();
+  });
+};
+
+changePictureSize();
+
+var textHashtags = document.querySelector('.text__hashtags');
+
+textHashtags.addEventListener('input', function () {
+  var textHashtagsValue = textHashtags.value;
+  var textLists = textHashtagsValue.toLowerCase().split(' ');
+
+  for (var i = 0; i < textLists.length; i++) {
+    if (!textLists[i].includes('#')) {
+      textHashtags.setCustomValidity('Хеш-тег должен начинаться с символа #');
+    } else if (!textLists[i].match(/(^#[a-zA-Zа-яА-Я0-9]*$)/g)) {
+      textHashtags.setCustomValidity('Cтрока после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы, эмодзи и т. д.')
+    } else if (textLists[i].length === MIN_LENGTH) {
+      textHashtags.setCustomValidity('Хеш-тег не может состоять только из одной решётки');
+    } else if (textLists[i].length > MAX_LENGTH) {
+      textHashtags.setCustomValidity('Максимальная длина одного хэш-тега  состовляет 20 символов включая решетку');
+    } else {
+      textHashtags.setCustomValidity('');
+    }
+  }
+});
